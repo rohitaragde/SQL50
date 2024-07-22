b17b91b62cf72e42c367090a0cae5108d2a2878e
Rohit Annasaheb Ragde <32512875+rohitaragde@users.noreply.github.com>
2024-07-22 18:32:59 -0400

    Update leetcode_1757.sql

--- leetcode_1757.sql
+++ leetcode_1757.sql
@@ -1,7 +1,9 @@
 --------------------- leetcode 1757 ----------------------------------------
 -------------- Recyclable and Low Fat products ---------------------------------
 
-able: Products
+**Question:-**
+
+Table: Products
 
 +-------------+---------+
 | Column Name | Type    |
@@ -44,6 +46,8 @@ Output:
 +-------------+
 Explanation: Only products 1 and 3 are both low fat and recyclable.
 use rohit;
+
+**Solution:-**
 
 create table Products001
 (product_id int primary key,
