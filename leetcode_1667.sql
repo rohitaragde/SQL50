64f8eeae9217ab1a0852dd3e1844c56d1c00978b
Rohit Annasaheb Ragde <32512875+rohitaragde@users.noreply.github.com>
2024-07-31 18:40:35 -0400

    Update leetcode_1667.sql

--- leetcode_1667.sql
+++ leetcode_1667.sql
@@ -1,4 +1,46 @@
 ----------------------------- Leetcode 1667-------------------------------------
+Question:-
+ 
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+Solution:-
+ 
 use rohit;
 
 create table Users16
@@ -14,11 +56,14 @@ create table Users16
  is uppercase and the rest are lowercase.
 Return the result table ordered by user_id.*/
 
+SQL Server:-
+
 select users_id,
 Upper(SUBSTRING(nname,1,1))+ lower(SUBSTRING(nname,2,len(nname)-1)) as name
 from Users16;
 
-
+MySQL:-
+ 
 select users_id,
 concat(Upper(SUBSTRING(nname,1,1)),lower(SUBSTRING(nname,2))) as name
 from Users16;
